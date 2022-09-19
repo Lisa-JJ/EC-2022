ab8ba2e90cf1c844caf6a16bd54cc7dc2a0ffe01
LenaEichhorn <77062239+LenaEichhorn@users.noreply.github.com>
2022-09-19 21:07:09 +0200

    test

--- dummy_demo.py
+++ dummy_demo.py
@@ -13,6 +13,8 @@ experiment_name = 'dummy_demo'
 if not os.path.exists(experiment_name):
     os.makedirs(experiment_name)
 
+print('Hello')
+
 # initializes environment with ai player using random controller, playing against static enemy
 env = Environment(experiment_name=experiment_name)
 env.play()
